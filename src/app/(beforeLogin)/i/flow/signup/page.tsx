9a90e7ab9baf369d503132ab8a41e10228c90332
sungdongyoon <ehdtjd0906@naver.com>
2024-03-31 16:09:10 +0900

    main 컴포넌트 및 css 모듈 생성

--- src/app/(beforeLogin)/i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from '../../../../page.module.css';
+import styles from '../../../_component/main.module.css';
 import SignupModal from '@/app/(beforeLogin)/_component/SignupModal';
 
 export default function Home() {
--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,30 +1,10 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import zLogo from '../../../../public/zlogo.png';
-import Link from 'next/link';
-import styles from '../../page.module.css';
+import Main from '../_component/Main';
 
 export default function Login() {
   const router = useRouter();
   router.replace('i/flow/login');
-  return (
-    <>
-      <div className={styles.left}>
-        <Image src={zLogo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }
