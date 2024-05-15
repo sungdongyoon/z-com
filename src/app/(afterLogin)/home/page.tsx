47797716c2cb4aa4ba4cfa03a43f621b62ff9a8a
sungdongyoon <ehdtjd0906@naver.com>
2024-05-15 11:49:10 +0900

    context api 세팅 / tab section

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,16 @@
+import PostForm from './_component/PostForm';
+import Tab from './_component/Tab';
+import TabProvider from './_component/TabProvider';
+import style from './home.module.css';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>홈 페이지</h1>
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        {/* <Post/> */}
+      </TabProvider>
     </main>
   );
 }
